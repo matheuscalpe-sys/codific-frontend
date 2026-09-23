e860fddba45a61ba8532765d5902e6abe6d04ea7
MATHEUS CAMARGO CALPE <matheus_calpe@estudante.sesisenai.org.br>
2026-09-23 16:57:55 -0300

    css

--- 01L-AlfaStudios/src/components/Header/header.jsx
+++ 01L-AlfaStudios/src/components/Header/header.jsx
@@ -1,9 +1,13 @@
+import "./header.css"
 
 function header(){
     return(
-        <header className="header">
+        <header className="Header">
+            <div className="header-container">
+
+            
             <div className="logo">
-                <img className="logo-icon" src="" alt="logo" />
+                <img className="logo-icon" src="../../../src/assets/imgs/360_F_950707390_EsXYNemHatWk8554bMJGADk2xXDActsU.jpg" alt="logo" />
                 <span className="logo-text">Studio Alfa</span>
             </div>
             <nav className="nav">
@@ -12,6 +16,7 @@ function header(){
                <a href="#">Sobre</a>
                <a href="#" className="btn-contatos">Contato</a>
             </nav>
+            </div>
         </header>
     )
 }
