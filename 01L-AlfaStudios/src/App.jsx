1dcb110c1a006896cc136b2306047ac3652185b6
MATHEUS CAMARGO CALPE <matheus_calpe@estudante.sesisenai.org.br>
2026-09-24 17:02:11 -0300

    parte do main obs: pedir ajuda pro luan pois não está funcionando

--- 01L-AlfaStudios/src/App.jsx
+++ 01L-AlfaStudios/src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header/header.jsx"
+import Main from "./components/Main/main.jsx"
 
 function app(){
   return(
     <>
     <Header></Header>
-    {/* <main></main>
-    <footer></footer>*/}
+    <main></main>
+    
     </>
   )
 }
---
+++ 01L-AlfaStudios/src/components/Main/main.jsx
@@ -0,0 +1,23 @@
+import './main.css'
+
+function main(){
+    return(
+        <main className='main'>
+            <section className='hero'>
+                <h1>Criamos sites que funcionam</h1>
+                <p>Layouts responsivos, rapidos e acessiveis para seu negocio crescer</p>
+
+                <div className='hero-buttons'>
+                    <a href="#orcamento" className='btn-primary'>peça seu orçamento</a>
+                    <a href="#orcamento" className='btn-secondary'>ver portifolio</a>
+                </div>
+            </section>
+
+            <section className='serviços'>
+
+            </section>
+        </main>
+    )
+}
+
+export default mainain
